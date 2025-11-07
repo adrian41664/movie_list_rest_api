f76e2a52c2811a5c74ae9688812ae7386aea9668
Adrian Walter <adrian-bremen@hotmail.de>
2025-11-07 15:26:45 +0100

    still initial commit

---
+++ src/main/java/de/adrianwalter/movie_list_rest_api/MovieListRestApiApplication.java
@@ -0,0 +1,13 @@
+package de.adrianwalter.movie_list_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieListRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieListRestApiApplication.class, args);
+	}
+
+}
